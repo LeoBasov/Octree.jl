2ddce79d55ecf751ba7dd65bfd199326c6cde954
LeoBasov <40352679+LeoBasov@users.noreply.github.com>
2025-06-01 21:45:57 +0200

    added base tree structure

---
+++ src/tree.jl
@@ -0,0 +1,110 @@
+struct Cuboid
+    xmin::Vector{AbstractFloat}
+    xmax::Vector{AbstractFloat}
+    center::Vector{AbstractFloat}
+    volume::AbstractFloat
+
+    function Cuboid(xmin, xmax)
+        new(float(xmin), float(xmax), 0.5 * (float(xmax) + float(xmin)), prod(float(xmax) - float(xmin)))
+    end
+end
+
+mutable struct Leaf
+    box::Cuboid
+    offset::Integer
+    n_elements::Integer
+    parent::Integer
+    children::Vector{Integer}
+
+    Leaf(box::Cuboid) = new(box, 0, 0, 0, [])
+end
+
+mutable struct Tree
+    elements::Vector{Integer}
+    leafs::Vector{Leaf}
+
+    Tree(root, positions) = new([i for i in eachindex(positions)], [root])
+end
+
+function build(positions)
+    root = _create_root(positions)
+    tree = Tree(root, positions)
+
+    _build_next_level!(tree, 1, positions)
+
+    return tree
+end
+
+function _create_root(positions)
+    box = _create_bounding_box(positions)
+    root = Leaf(box)
+
+    root.offset = 0
+    root.n_elements = length(positions)
+    root.parent = 0
+    root.children = []
+
+    return root
+end
+
+function _create_bounding_box(positions)
+    xmin = [Inf, Inf, Inf]
+    xmax = [-Inf, -Inf, -Inf]
+
+    for pos in positions
+        for i in 1:3
+            xmin[i] = pos[i] < xmin[i] ? pos[i] : xmin[i]
+            xmax[i] = pos[i] > xmax[i] ? pos[i] : xmax[i]
+        end
+    end
+
+    return Cuboid(xmin, xmax)
+end
+
+function _build_next_level!(tree, parent_id, positions)
+    parent = tree.leafs[parent_id]
+
+    # dummy check
+    if parent.n_elements > 10
+        return
+    end
+
+    boxes = _create_children_boxes(parent.box)
+    offset = parent.offset
+    runner_idx = parent.offset
+
+    resize!(tree.leafs, length(tree.leafs) + 8)
+    resize!(parent.children, 8)
+
+    for i in 1:8
+        leaf = Leaf(boxes[i])
+        leaf.parent = parent_id
+        leaf.offset = offset
+
+        for elem_id in runner_idx:parent.offset + parent.n_elements
+            if is_in_box(leaf.box, positions[tree.elements[elem_id]])
+                #TODO
+            end
+        end
+    end
+end
+
+function _create_children_boxes(box::Cuboid)
+    box1 = Cuboid(box.xmin, box.center)
+    box2 = Cuboid([box.center[1], box.xmin[2], box.xmin[3]], [box.xmax[1], box.center[2], box.center[3]])
+    box3 = Cuboid([box.center[1], box.center[2], box.xmin[3]], [box.xmax[1], box.xmax[2], box.center[3]])
+    box4 = Cuboid([box.xmin[1], box.center[2], box.xmin[3]], [box.center[1], box.xmax[2], box.center[3]])
+
+    box5 = Cuboid([box.xmin[1], box.xmin[2], box.center[3]], [box.center[1], box.center[2], box.xmax[3]])
+    box6 = Cuboid([box.center[1], box.xmin[2], box.center[3]], [box.xmax[1], box.center[2], box.xmax[3]])
+    box7 = Cuboid([box.center[1], box.center[2], box.center[3]], [box.xmax[1], box.xmax[2], box.xmax[3]])
+    box8 = Cuboid([box.xmin[1], box.center[2], box.center[3]], [box.center[1], box.xmax[2], box.xmax[3]])
+end
+
+function is_in_box(box::Cuboid, pos::Vector{AbstractFloat})
+    inx = pos[1] <= box.xmax[1] && pos[1] >= box.xmin[1]
+    iny = pos[2] <= box.xmax[2] && pos[2] >= box.xmin[2]
+    inz = pos[3] <= box.xmax[3] && pos[3] >= box.xmin[3]
+
+    return inx && iny && inz
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,32 @@
 using Octree
 using Test
 
-@testset "Octree.jl" begin
-    @test Octree.greet() == "Hello World!"
+function create_positions(N, pos_min = [-1.0, -2.0, -3.0], pos_max = [4.0, 5.0, 6.0])
+    positions = Vector{Vector{Float64}}(undef, N)
+    diff = pos_max - pos_min
+
+    for i = 1:N - 1
+        positions[i] = pos_min + rand(3).*diff
+    end
+
+    positions[N - 1] = pos_min
+    positions[N] = pos_max
+
+    return positions
+end
+
+@testset "tree.jl" begin
+    N_positions = 100
+    positions = create_positions(N_positions)
+    tree = Octree.build(positions)
+    root = tree.leafs[1]
+
+    @test root.box.xmin == [-1.0, -2.0, -3.0]
+    @test root.box.xmax == [4.0, 5.0, 6.0]
+    @test root.box.center == [1.5, 1.5, 1.5]
+    @test root.box.volume == 5.0 * 7.0 * 9.0
+
+    @test root.offset == 0
+    @test root.n_elements == N_positions
+    @test root.parent == 0
 end
