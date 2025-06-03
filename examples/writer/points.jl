a062ad45799352ce447c927c13c0ba709b8f27be
Leo Basov <leo.basov@dlr.de>
2025-06-03 13:39:54 +0200

    added custom points test case

---
+++ examples/writer/points.jl
@@ -0,0 +1,36 @@
+using Octree
+
+function read_points(file_name)
+    points = []
+
+    open(file_name) do file
+        while !eof(file)
+            line = readline(file)
+            splt = split(line, " ")
+
+            if splt[1] == "#"
+                continue
+            elseif length(splt) == 3
+                point = [parse(Float64, splt[1]), parse(Float64, splt[2]), parse(Float64, splt[3])]
+                push!(points, point)
+            end
+        end
+    end
+
+    return points
+end
+
+config = Config()
+
+config.aspect_ratio = 1.5
+
+file_name = "examples/writer/points_cylinder.asc"
+points = read_points(file_name)
+
+println(length(points))
+
+tree = build(points; config = config)
+
+write_vtk("vtk_test", tree)
+
+println("done")
